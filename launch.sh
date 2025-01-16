58c3ea1161d9ee53e9a67685f9bdd7ce98ad42e8
Konrad Pagacz <konrad.pagacz@gmail.com>
2025-01-16 17:43:30 +0100

    improve launch script

--- launch.sh
+++ launch.sh
@@ -4,9 +4,11 @@ cp nginx.conf /etc/nginx/nginx.conf
 sudo nginx -s reload
 
 cd heist
+cargo build . --release
 cargo run --release &
 echo "Launched backend"
 
 cd ../getaway
+cargo build . --release
 cargo run --release &
 echo "Launched frontend"
