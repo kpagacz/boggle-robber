08b5e8ab3a324e100a939793f05454a286ea9c02
Konrad Pagacz <konrad.pagacz@gmail.com>
2025-12-14 12:01:14 +0100

    fix: fixed Cross configuratio
    * Fixed cross configuration to actually compile against libssl1.1
      instead of libssl1.0.0 because libssl1.0.0 is no longer available
      on ubuntu24.04 where I host the application, which caused
      failure to run (reporting missing dynamic libraries).

--- deploy_to_ec2.sh
+++ deploy_to_ec2.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 INST="52.59.249.245"
+IDENTITY="~/.ssh/WjazdDoCentraliKurwa"
 
-ssh -i "Wjazd do Centrali Kurwa" "ubuntu@${INST}" "/bin/bash -s" <<EOF
+ssh -i ${IDENTITY} "ubuntu@${INST}" "/bin/bash -s" <<EOF
     pkill heist
     pkill getaway
     exit
 EOF
 
 # Copy the backend binary to the INST host using scp
-scp -i "Wjazd do Centrali Kurwa" ./heist/target/aarch64-unknown-linux-gnu/release/heist "ubuntu@${INST}":/home/ubuntu/heist
+scp -i ${IDENTITY} ./heist/target/aarch64-unknown-linux-gnu/release/heist "ubuntu@${INST}":/home/ubuntu/heist
 
 # Copy the frontend binary to the INST host using scp
-scp -i "Wjazd do Centrali Kurwa" ./getaway/target/aarch64-unknown-linux-gnu/release/getaway "ubuntu@${INST}":/home/ubuntu/getaway
+scp -i ${IDENTITY} ./getaway/target/aarch64-unknown-linux-gnu/release/getaway "ubuntu@${INST}":/home/ubuntu/getaway
 
 # Log into the INST host using ssh and kill the running backend and frontend processes
 # then run the new backend and frontend binaries
-ssh -tt -i "Wjazd do Centrali Kurwa" "ubuntu@${INST}" "/bin/bash -s" <<EOF
+ssh -tt -i ${IDENTITY} "ubuntu@${INST}" "/bin/bash -s" <<EOF
     nohup /home/ubuntu/heist & sleep 1;
     nohup /home/ubuntu/getaway & sleep 1;
     exit
